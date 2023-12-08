b66aa494f03bc040e28da437f8bbdc6bbf3c47ed
Muhammad <ahmadmuhdpk@gmail.com>
2023-12-09 02:19:10 +0500

    build added

---
+++ client/build/precache-manifest.734820ae24ad4edf1b65489e197ba1af.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef31c9deab680cfc53e18708405c0d52",
+    "url": "/index.html"
+  },
+  {
+    "revision": "feafee8a6e623dd64f40",
+    "url": "/static/css/main.9516a8fc.chunk.css"
+  },
+  {
+    "revision": "5597f0e96a2e6e179735",
+    "url": "/static/js/2.ffd3900a.chunk.js"
+  },
+  {
+    "revision": "eca373d6688a5d5efead34244f081e8b",
+    "url": "/static/js/2.ffd3900a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "feafee8a6e623dd64f40",
+    "url": "/static/js/main.09ffc744.chunk.js"
+  },
+  {
+    "revision": "f620b634b443fc7d2dfb",
+    "url": "/static/js/runtime-main.7e6e88a6.js"
+  }
+]);
